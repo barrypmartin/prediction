43985fd97cfe12c89e94b548d4d99a2d218e2350
Barry Martin <125478596+barrypmartin@users.noreply.github.com>
2023-02-27 08:18:30 -0700

    Add files via upload

---
+++ prediction.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 27 05:20:58 2023
+
+@author: barry
+"""
+
+import streamlit as st
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+#from sklearn.ensemble import Hist_Gradient_Boosting_Classifier
+
+def main():
+    st.title("Heart Health Predictor")
+    
+    # Upload CSV file
+    uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+    if uploaded_file is not None:
+        # Convert CSV file to pandas dataframe
+        df = pd.read_csv(uploaded_file)
+        
+        # Show the first 10 rows of the dataframe
+        st.write("First 10 rows of the uploaded file:")
+        st.write(df.head(10))
+        
+        # Split data into features and target
+        X = df.iloc[:, :-1]
+        y = df.iloc[:, -1]
+        
+        # Split data into training and testing sets
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+        
+        # Create a logistic regression object and fit to training data
+        logreg = LogisticRegression()
+        logreg.fit(X_train, y_train)
+        
+        # Make predictions on testing data and calculate accuracy
+        y_pred = logreg.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        
+        # Show accuracy score
+        st.write("Percent chance of a Heart Attack:", accuracy)
+
+if __name__ == "__main__":
+    main()
