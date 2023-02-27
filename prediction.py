9cc6755f8db7b5563d9f62420ddfbc5b5cfcbf4a
Barry Martin <125478596+barrypmartin@users.noreply.github.com>
2023-02-27 13:12:26 -0700

    Add files via upload

--- prediction.py
+++ prediction.py
@@ -32,7 +32,7 @@ def main():
         y = df.iloc[:, -1]
         
         # Split data into training and testing sets
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.5,test_size=0.3, random_state=42)
         
         # Create a logistic regression object and fit to training data
         logreg = LogisticRegression()
